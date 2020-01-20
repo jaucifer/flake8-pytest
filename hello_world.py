a874c0ac4d72ee7d93f8209a54ad70e38b1066c7
Jason Wong <59460466+jaucifer@users.noreply.github.com>
2020-01-20 12:35:08 +0800

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,20 @@
 from flask import Flask
 
+
 def wrap_html(greet):
     html = format(greet)
     return html
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     greet = 'Welcome to CI/CD'
     html = wrap_html(greet)
     return html
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
