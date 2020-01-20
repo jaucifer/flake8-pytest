ae75c88e6e7c703c5bc57df284c6a3f62305e5a9
Jason Wong <59460466+jaucifer@users.noreply.github.com>
2020-01-20 12:37:36 +0800

    Update test_hello_world.py

--- test_hello_world.py
+++ test_hello_world.py
@@ -3,19 +3,19 @@ import unittest
 
 
 class TestHelloWorld(unittest.TestCase):
-  def setUp(self):
+    def setUp(self):
         self.app = hello_world.app.test_client()
         self.app.testing = True
 
-  def test_status_code(self):
+    def test_status_code(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
 
-  def test_greeting_message(self):
-    response = self.app.get('/')
-    greet = b'Welcome to CI/CD'
-    self.assertEqual(response.data, greet)
+    def test_greeting_message(self):
+        response = self.app.get('/')
+        greet = b'Welcome to CI/CD'
+        self.assertEqual(response.data, greet)
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
