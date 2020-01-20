34b1d41d65c66bd151af3f6f07554f07cf69de49
Jason Wong <59460466+jaucifer@users.noreply.github.com>
2020-01-20 10:38:19 +0800

    Update test_hello_world.py

--- test_hello_world.py
+++ test_hello_world.py
@@ -3,7 +3,7 @@ import unittest
 
 
 class TestHelloWorld(unittest.TestCase):
-  def setUp(self):
+  def setup(self):
         self.app = hello_world.app.test_client()
         self.app.testing = True
 
@@ -19,4 +19,3 @@ class TestHelloWorld(unittest.TestCase):
 
 if __name__ == '__main__':
   unittest.main()
-
